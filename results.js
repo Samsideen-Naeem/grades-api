a66e6c9119fd0a79799d7a7cf1bd9a1e3c08d614
Samsideen-Naeem <naeemsamsideen618@gmail.com>
2022-09-01 14:07:57 +0000

    first commit

---
+++ results.js
@@ -0,0 +1,9 @@
+const results = [{ name:"Samsideen Naeem", class:"form three", grade: "8"},
+                 { name:"Atif Amu", class:"form three", grade: "30"},
+                 { name:"Kashaf Abubakari", class:"form three", grade: "52"},
+                 { name:"Adinatu Adams", class:"form three", grade: "9"},
+                 { name:"Shahada Banasing", class:"form three", grade: "8"},
+                 {name:"Ail Mohammede Subur", class:"form three", grade:"44"}
+]
+
+module.exports = results
